c2051838a93211bc99075aab3456ade71a65209c
Luciano Escalante <75239876+Badghel@users.noreply.github.com>
2021-10-08 03:07:20 -0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@
 let active = document.getElementById("active");
 let menu = document.getElementById("menu");
 let atras = document.getElementById("atras");
+let links = document.querySelectorAll(".menu__link");
 active.addEventListener("click",()=>{
     menu.style.display="unset";
     active.classList.add("disable")
@@ -10,3 +11,11 @@ atras.addEventListener("click",()=>{
     menu.style.display="none";
     active.classList.remove("disable");
 })
+
+for(i=0;i< links.length;i++){
+    links[i].addEventListener("click",()=>{
+        menu.style.display="none";
+        active.classList.remove("disable");
+    })
+};
+
